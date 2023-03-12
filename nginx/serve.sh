80785bf2d9c66b0cc2abfff70b298dffa765f7fc
Stefan Kochev <stefan_kochev@yahoo.com>
2023-03-12 20:44:46 +0100

    win fixes

--- nginx/serve.sh
+++ nginx/serve.sh
@@ -3,5 +3,6 @@
 
 echo "Setting up nginx environment ..."
 envsubst < /api_nginx.conf.template > /etc/nginx/sites-enabled/api_nginx.conf
+
 echo "Starting nginx server ..."
 exec nginx -g "daemon off;"
