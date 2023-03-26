c130bbb40dc3916b5fc4e4048bdd49fe9ad67b53
Stefan Kochev <stefan_kochev@yahoo.com>
2023-03-26 15:19:49 +0200

    Logger config & minor updates

--- notifications-service/src/pubsub/handlers/new_items_handler.py
+++ notifications-service/src/pubsub/handlers/new_items_handler.py
@@ -1,7 +1,8 @@
+import asyncio
 from fastapi.logger import logger
 
 
-def new_items_handler(message: dict):
+async def new_items_handler(message: dict):
     """Handler of new_item messages.
 
     :param message: the messaging containing the new_item event
@@ -9,6 +10,6 @@ def new_items_handler(message: dict):
     """
     # Here we could be sending mobile push notifications, email notifications or notify users
     # using other communication channels (here we just mock/log this action, not implemented)
-    logger.info("Sending notifications")
-    print("Handling message")
-    print(message)
+    logger.info(f"Handling message: {message} - Sending notifications...")
+    await asyncio.sleep(10)  # mock sending notifications
+    logger.info(f"Event processed: {message}")
